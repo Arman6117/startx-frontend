13b19a1dc4e4290d97149a5050aa749d1f8d8bdb
Arman Patel <vanixgaming634@gmail.com>
2025-12-18 21:11:55 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+
 import './App.css';
 import { Outlet } from 'react-router-dom';
-import Navbar from './components/Navbar';
 import ChatAssistant from './components/ChatAssistant'; // Import the chat assistant
 import AppContext from './utils/Context';
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <AppContext>
